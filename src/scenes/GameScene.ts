66010f4c6d3bbfad991babd1a323b1329b607a15
eh <ye@h.com>
2026-08-09 22:07:52 +0000

    w-add stats

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -3,12 +3,14 @@ import { Card } from '../objects/Card'
 import Board from '../objects/Board'
 import type { CardData } from '../game/types'
 
-const socket = new WebSocket('ws://192.168.100.40:8080')
-
 export default class GameScene extends Phaser.Scene {
 
     private board!: Board
     private cards: Card[] = []
+    private socket!: WebSocket
+    private playerId!: string
+    private statsText!: Phaser.GameObjects.Text
+    private displayName!: string
 
     constructor() {
         super('GameScene')
@@ -19,7 +21,7 @@ export default class GameScene extends Phaser.Scene {
 
         this.cards.forEach((card, index) => {
             card.onClick(() => {
-                socket.send(JSON.stringify({
+                this.socket.send(JSON.stringify({
                     type: 'flipCard',
                     index,
                 }))
@@ -27,15 +29,76 @@ export default class GameScene extends Phaser.Scene {
         })
     }
 
-    create() {
-        this.board = new Board(this)
+create() {
+    this.board = new Board(this)
 
-        socket.onmessage = (event) => {
-            const message = JSON.parse(event.data)
+    this.playerId =
+        localStorage.getItem('playerId') ??
+        `player-${Date.now()}-${Math.random()
+            .toString(36)
+            .slice(2)}`
 
-            if (message.type !== 'gameState') {
-                return
-            }
+    localStorage.setItem(
+        'playerId',
+        this.playerId
+    )
+
+    this.displayName =
+        localStorage.getItem('displayName') ??
+        `Guest#${Math.random()
+            .toString(36)
+            .substring(2, 6)
+            .toUpperCase()}`
+
+    localStorage.setItem(
+        'displayName',
+        this.displayName
+    )
+
+    this.statsText = this.add.text(
+        20,
+        20,
+        `${this.displayName}\nFlips: 0\nMatches: 0`,
+        {
+            fontSize: '20px',
+            color: '#ffffff',
+        }
+    )
+
+
+        this.socket = new WebSocket(
+            'ws://192.168.100.40:8080'
+        )
+
+this.socket.onopen = () => {
+    console.log('WebSocket connected')
+
+this.socket.send(JSON.stringify({
+    type: 'identify',
+    playerId: this.playerId,
+}))
+this.socket.send(JSON.stringify({
+    type: 'getStats',
+}))
+}
+
+
+this.socket.onmessage = (event) => {
+    const message = JSON.parse(event.data)
+
+if (message.type === 'playerStats') {
+    this.statsText.setText(
+        `${this.displayName}\n` +
+        `Flips: ${message.stats.flips}\n` +
+        `Matches: ${message.stats.matches}`
+    )
+
+    return
+}
+
+    if (message.type !== 'gameState') {
+        return
+    }
 
             console.log('Server state:', message.state)
 
@@ -60,6 +123,19 @@ export default class GameScene extends Phaser.Scene {
                     }
                 }
             })
+        }
+
+        this.socket.onerror = (error) => {
+            console.error(
+                'WebSocket error:',
+                error
+            )
+        }
+
+        this.socket.onclose = () => {
+            console.log(
+                'WebSocket disconnected'
+            )
         }
     }
 }
